b3a7e7474863dfcd84c901669c1497abb41f92fb
tarrenj <tarrenj@users.noreply.github.com>
2019-01-26 12:46:01 -0500

    Update getdocker.sh

--- getdocker.sh
+++ getdocker.sh
@@ -24,7 +24,7 @@ apt-get install -y \
     software-properties-common
 
 # Get the signing key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # Add the repo
 apt-add-repository \
