52d2f3551d902ff3ddb2eac163d26d0e1a7bd765
Jake Tarren <jake.tarren@gmail.com>
2023-09-23 20:05:08 -0400

    Add terminal config

--- fastup.sh
+++ fastup.sh
@@ -3,10 +3,11 @@
 
 # A dumb script to speed up setup of things
 
-/usr/bin/sudo /usr/bin/apt -y install git vim zsh tmux nmap build-essential curl wget cmake sshuttle python3 python3-dev python3-pip
+/usr/bin/sudo /usr/bin/apt -y install git vim zsh tmux nmap build-essential curl wget cmake sshuttle python3 python3-dev python3-pip alacritty
 
 # Flat/easy dot files
 cp ~/builds/dotfiles/.tmux.conf ~/.tmux.conf
+cp ~/builds/dotfiles/.alacritty.conf ~/.alacritty.conf
 
 # Git config (with SSH signing key)
 cp ~/builds/dotfiles/.gitconfig ~/.gitconfig
